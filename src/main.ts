77796ab8e8611a0873e473c634ac6d864b7d0b30
Moll Jonathan <jonllom80@gmail.com>
2021-09-03 14:26:45 +1100

    fri14

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,30 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBuOmJu6vKrllq87EAqN4ffYFd6E-n1gKM",
+  authDomain: "hex-journal.firebaseapp.com",
+  projectId: "hex-journal",
+  storageBucket: "hex-journal.appspot.com",
+  messagingSenderId: "504357681758",
+  appId: "1:504357681758:web:d90a1e0ff19a4212e092cf",
+  measurementId: "G-LS77X4PRT8"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);  
+
+
 if (environment.production) {
   enableProdMode();
 }
